135b94e5e9809e5cdd8b8605cd48fe5a46214f25
Jordie <xd@jrdie.nl>
2022-03-05 20:35:10 +0100

    Rework index.tsx
    [skip ci]

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 
-const Home: NextPage = () => {
+export default function Home(): JSX.Element {
   return (
     <div>
       <Head>
@@ -20,6 +19,4 @@ const Home: NextPage = () => {
       </footer>
     </div>
   );
-};
-
-export default Home;
+}
