f4c3c2a53c4eafa12da7af198253b6818445ad05
Jordie <xd@jrdie.nl>
2022-03-06 17:48:51 +0100

    Add page with a lot of text for testing

---
+++ src/pages/longpage.tsx
@@ -0,0 +1,16 @@
+export default function Longpage(): JSX.Element {
+  return (
+    <>
+      {Array.from({ length: 20 }).map((_, i) => {
+        return (
+          <p key={i}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+            dolore facilis fuga magnam, numquam quasi quos. Accusamus, deleniti,
+            dolorem esse iure libero nam quas quis reiciendis reprehenderit
+            sapiente veritatis vero!
+          </p>
+        );
+      })}
+    </>
+  );
+}
